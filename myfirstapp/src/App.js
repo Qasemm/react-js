e971d07fe5b641c59732eea9917b1cc7c20313a6
qasemm <qaseem.com34@gmail.com>
2020-02-07 23:26:36 +0200

    go00

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
-import Child from './Child';
+
 class App extends Component{
+  state={
+    name:'Qasem',
+    age:27
+  }
+ Change= ()=>{
+    this.setState({
+      name:'Mohammad',
+      age:28
+    })
+  }
   render(){
     return(
       <div className="App">
-        <Child/>
+        <button onClick={this.Change}>Click</button>
+        <p>{this.state.name}</p>
+        <p>{this.state.age}</p>
       </div>
     )
   }
