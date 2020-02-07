70b825e98af21aba1968698d3107512d28b7360c
qasemm <qaseem.com34@gmail.com>
2020-02-07 22:47:46 +0200

    go00

---
+++ myfirstapp/src/Child.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Child extends Component{
+    render(){
+        return(
+            <div>
+                Child Component
+            </div>
+        )
+    }
+}
+export default Child;
---
+++ myfirstapp/src/module.js
@@ -0,0 +1,16 @@
+export const x= 2;
+
+export const y = 3;
+
+export function aler(){
+    alert('hello');
+}
+
+
+function aler3(){
+    console.log('hi React');
+}
+
+
+export default aler3
+
